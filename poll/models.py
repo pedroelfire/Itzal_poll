a3c3dd3e7b36fe5182bc13aea5cdd9c918b91c6f
Pedroelfire <pedroelfire@gmail.com>
2024-10-03 19:23:48 -0600

    Campo solicitud agregado

--- poll/models.py
+++ poll/models.py
@@ -4,6 +4,7 @@ class Contact(models.Model):
     correo = models.EmailField(max_length=255)
     asunto = models.CharField(max_length=255)
     mensaje = models.TextField()
+    solicitud = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
